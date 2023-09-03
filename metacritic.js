44d2d4dd06363e0ddadfd5061f91dc43fc29cdd3
WildineyDi Masi <wildiney@gmail.com>
2023-09-03 18:46:12 -0300

    refactoring

--- metacritic.js
+++
@@ -1,1 +0,0 @@
-javascript: window.location = "https://www.metacritic.com/game/playstation-4/" + encodeURI(prompt("Game Name").replaceAll("&", "").replaceAll(":", "").replaceAll(" ", "-").replaceAll("--", "-")).toLowerCase();
