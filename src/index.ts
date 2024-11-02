3c68ad06eb66271098c4af5b1b481b22150a7523
Wildiney Di Masi <wildiney@gmail.com>
2024-11-02 20:55:36 -0300

    feat(index): update main url

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import MCritic from './classes/mcritic'
 
-const url = 'https://www.metacritic.com/game/playstation-4/'
+const url = 'https://www.metacritic.com/game/'
 const meta = new MCritic(url)
 const game = meta.getTextSelectionOrPrompt()
 const newURL = meta.getFullURLFor(game)
